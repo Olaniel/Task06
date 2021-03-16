b64beb6297050954980fa5410017c37859d8eade
Olaniel <olaniel@mail.ru>
2021-03-16 06:16:02 +0300

    Update Oven.java

--- jc1-les24-examples-criteria/src/by/tc/task01/entity/Oven.java
+++ jc1-les24-examples-criteria/src/by/tc/task01/entity/Oven.java
@@ -1,6 +1,9 @@
 package by.tc.task01.entity;
 
-public class Oven extends Appliance{
+public class Oven extends Appliance{// Дима, увы, это не иерерхия. Более того, эту ошибку мы разбирали на занятии. Получается твое наследование вот в таком виде бесполезно, производные классы друг от друга не отличаются
+	// схалявил, так нельзя(
+	
+	// а про equals, hashCode и toString я вообще молчу(
 
 	public Oven() {
 	
